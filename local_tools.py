6b701dc74762d5f46939272036cbd8904523a30e
Galen Curwen-McAdams <galencm@fastmail.com>
2018-01-14 05:05:05 +0000

    Problem: no function to get scheduled jobs status
    Solution: add function that parses machine yaml and
    looks up scheduler(currently nomad) jobs. Return a
    dictionary {name:status,...}

--- local_tools.py
+++ local_tools.py
@@ -37,3 +37,71 @@ def rpc(func,*args):
     zc.connect("tcp://{}:{}".format(rip,rport))
     result = zc(func,*args)
     return result
+
+def parse_job_output(job_string):
+    jobs = {}
+    for line_num,line in enumerate(job_string.split("\n")):
+        if line_num > 0 and line:
+            # remove extra whitespace between columns
+            job_name,job_type,job_priority,job_status,_ = " ".join(line.split()).split(" ",4)
+            jobs[job_name] = job_status
+    return jobs
+
+def lookup_machine(machine_yaml_file,ignored_services=None):
+    """
+    return a dict {service_name:status}
+    ignored_services are not included in dict
+    """
+    import subprocess
+    from ruamel.yaml import YAML
+    import pathlib
+    # start.sh
+    # --tags machine machine_core
+    # but not in yaml?
+    # get all with machine tag
+    # parse yaml for includes, check...
+
+    if ignored_services is None:
+        ignored_services = []
+
+    nomad_ip,nomad_port = lookup("nomad")
+
+    nomad_jobs = subprocess.check_output(["nomad",
+                                         "status",
+                                         "-address=http://{}:{}".format(nomad_ip,nomad_port)
+                                         ]).decode()
+
+    nomad_jobs = parse_job_output(nomad_jobs)
+
+    yaml=YAML(typ='safe')
+    machine_outline = yaml.load(pathlib.Path(machine_yaml_file))
+    #print(machine_outline)
+    include_names = set()
+    for includes in machine_outline['includes']:
+        for include,include_properties in includes.items():
+            # use name property if available, otherwise
+            # name will be key
+            try:
+                include_names.add(include_properties['name'])
+            except:
+                # currently external file properties
+                # are not available via yaml, 
+                # so the name chop off .hcl and see
+                #
+                # machine.py removes .py ending?
+                if include.endswith(".hcl"):
+                    include_names.add(include[:-4])
+                elif include.endswith(".py"):
+                    include_names.add(include[:-3])
+                else:
+                    include_names.add(include)
+
+    #assert include_names.issubset(set(nomad_jobs.keys()))
+    machine_scheduled_snapshot = {k:None for k in set(include_names)-set(ignored_services)}
+    for k,v in nomad_jobs.items():
+        if k in include_names and k not in ignored_services:
+            #assert v == "running"
+            print("{} {}".format(k,v))
+            snapshot[k] = v
+
+    return machine_scheduled_snapshot
